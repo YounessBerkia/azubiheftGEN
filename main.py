43c8145efed0cf8c3074746a3b71c632c92a7484
Berkia <berkiayouness@gmail.com>
2026-08-17 09:26:29 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from web.app import create_app
 from config import FLASK_HOST, FLASK_PORT
 
-app = create_app()
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(
         host=FLASK_HOST,
         port=FLASK_PORT,
